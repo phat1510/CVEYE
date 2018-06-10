7c98c9b63ea43eaad2b381a0e5fdb10ccad47482
PhatDo <dominhphat93@gmail.com>
2018-06-10 19:09:08 +0700

    changed form language

--- CVEYEV1/Main.Designer.cs
+++ CVEYEV1/Main.Designer.cs
@@ -127,7 +127,7 @@
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Location = new System.Drawing.Point(9, 21);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(112, 30);
+            this.button2.Size = new System.Drawing.Size(99, 30);
             this.button2.TabIndex = 8;
             this.button2.Text = "Chụp ảnh";
             this.button2.UseVisualStyleBackColor = true;
@@ -136,11 +136,11 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(127, 21);
+            this.button1.Location = new System.Drawing.Point(114, 21);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(92, 30);
+            this.button1.Size = new System.Drawing.Size(105, 30);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Video";
+            this.button1.Text = "Bật camera";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Video_Click);
             // 
